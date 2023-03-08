4504ef8687e6e2a8798a9cd74af52022355269ac
Mwadime <stanleymwadime@gmail.com>
2023-03-08 13:49:27 -0800

    tasks added

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,22 +6,28 @@
  * @s2: 2nd string caintaing '*' or not
  * Return: 1 if the strings can be considered as identical, 0 otherwise
  */
-int wildcmp(char *s1, char *s2)
-{
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '\0')
-		return (0);
-	if (*s2 == '*')
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	if (*s1 == '\0')
-	{
-		if (*s2 != '*')
-			return (0);
-		else
-			return (wildcmp(s1, s2 + 1));
-	}
-	return (0);
+int wildcmp(char *s1, char *s2) {
+    
+    if (*s1 == '\0' && *s2 == '\0') {
+        return 1;
+    }
+    
+    if (*s2 == '*') {
+        
+        if (wildcmp(s1, s2 + 1)) {
+            return 1;
+        }
+        
+        while (*s1 != '\0') {
+            if (wildcmp(s1 + 1, s2)) {
+                return 1;
+            }
+            s1++;
+        }
+    }
+    
+    if (*s1 == *s2) {
+        return wildcmp(s1 + 1, s2 + 1);
+    }
+    return 0;
 }
